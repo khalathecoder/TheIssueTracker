5a36b9f1e5e0d32f48f16434bfc219aab276d50a
Khala Wright <khalawright5@gmail.com>
2023-06-14 12:00:42 -0400

    Added Dashboard and Landing page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,13 +23,18 @@ namespace TheIssueTracker.Controllers
             return View();
         }
 
-        public IActionResult HomeDashboard()
+        public IActionResult Dashboard()
         {
             return View();
         }
 
+		public IActionResult Landing()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
